c63fcf2752fd68c3a6d17642ec0abbf03a18dd17
e2k-vampyre <vip.elick@gmail.com>
2021-03-13 20:32:25 +0500

    add gui

--- main.py
+++ main.py
@@ -1,29 +1,62 @@
+import sys
 import os
-path = os.getcwd()
-print("Текущая директория: ",path)
-a = input("Изменить? д/н\n").lower()
-while((a[0] != 'д') and (a[0] != 'н')):
-    a = input("Изменить? д/н\n").lower()
-    if (a[0] == 'д'):
-        tmp = input()
-        while(not os.path.exists(tmp)):
-            tmp = input("Неправильный ввод\n")
-        path = tmp     
-lst = os.listdir(path)
-for i in lst:
-    a = i.split('.')
-    if len(a) < 2:
-        continue
-    if a[1] == "jpg" and not os.path.exists("Picters"):
-        os.mkdir('Picters')
-    elif a[1] == "webm" and not os.path.exists("Videos"):
-        os.mkdir('Videos')    
-for i in lst:
-    a = i.split('.')
-    if len(a) < 2:
-        continue
-    if a[1] == "jpg":
-        os.replace(i,'Picters/'+i)
-    elif a[1] == "webm":
-        os.replace(i,'Videos/'+i)
-print("Все файлы были перемещены по папкам")        
+
+from PyQt5 import QtWidgets
+
+import design
+
+class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
+    dir1 = os.getcwd()
+    dir2 = os.getcwd()
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self) 
+        self.pushButton.clicked.connect(self.browse_folder1)
+        self.pushButton_2.clicked.connect(self.browse_folder2) 
+        self.pushButton_3.clicked.connect(self.accept) 
+
+    def browse_folder1(self):
+        directory1 = QtWidgets.QFileDialog.getExistingDirectory(self, "Выберите папку")
+
+        if directory1:
+            self.listWidget.clear()
+            self.dir1 = directory1
+            self.listWidget.addItem("Текущая директория: "+directory1)
+
+    def browse_folder2(self):
+        directory2 = QtWidgets.QFileDialog.getExistingDirectory(self, "Выберите папку")
+
+        if directory2:
+            self.listWidget_2.clear()
+            self.dir2 = directory2
+            self.listWidget_2.addItem("Текущая директория: "+directory2)
+
+    def accept(self):
+        lst = os.listdir(self.dir1)
+        for i in lst:
+            a = i.split('.')
+            if len(a) < 2:
+                continue
+            if a[1] == "jpg" and not os.path.exists(self.dir2+"/Picters"):
+                os.mkdir(self.dir2+"/Picters")
+            elif a[1] == "webm" and not os.path.exists(self.dir2+"/Video"):
+                os.mkdir(self.dir2+"/Video")
+
+        for i in lst:
+            if len(a) < 2:
+                continue
+            if a[1] == "jpg":
+                os.replace(self.dir1+"/"+i,self.dir2+"/Picters/"+i)
+            elif a[1] == "webm":
+                os.replace(self.dir1+"/"+i,self.dir2+"/Video/"+i)         
+
+          
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = ExampleApp()
+    window.show()
+    app.exec_()
+
+if __name__ == '__main__':
+    main()
