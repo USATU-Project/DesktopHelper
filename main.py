8dab3984e420f69c209603b389525ea3ebc3b11d
e2k-vampyre <vip.elick@gmail.com>
2021-04-09 19:56:13 +0500

    ready first window

--- main.py
+++ main.py
@@ -1,38 +1,56 @@
 import sys
 import os
 
-from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import QCoreApplication
 
-from ui import folderselect
-from ui import settings
+from ui import folderselect, settings, logs
 
 def_dir = os.getcwd()
 
 
-class MainApp(QtWidgets.QMainWindow, folderselect.Ui_MainWindow):
+class MainApp(QMainWindow, folderselect.Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.folder_select.setText(def_dir)
         self.settings_button.clicked.connect(self.settings_window)
         self.select_button.clicked.connect(self.fold_sel)
+        self.accept_button.clicked.connect(self.accept_but)
+        self.cancel_button.clicked.connect(QCoreApplication.instance().quit)
 
     def settings_window(self):
         self.w2 = Settings()
         self.w2.show()
         
     def fold_sel(self):
-        directory = QtWidgets.QFileDialog.getExistingDirectory(self, "Выберите папку...")
+        directory = QFileDialog.getExistingDirectory(self, "Выберите папку...")
+        if (directory != ""):
+            self.folder_select.setText(directory)
+    
+    def set_dir(self):
         global def_dir
+        directory = self.folder_select.toPlainText()
         def_dir = directory
-        self.folder_select.setText(directory)
 
-class Settings(QtWidgets.QMainWindow, settings.Ui_MainWindow):
+    def accept_but(self):
+        self.set_dir()
+        self.w3 = LogWindow()
+        self.w3.show()
+
+class Settings(QMainWindow, settings.Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        
+
+class LogWindow(QMainWindow, logs.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self) 
 
 def main():
-    app = QtWidgets.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     window = MainApp()
     window.show()
     app.exec_()
