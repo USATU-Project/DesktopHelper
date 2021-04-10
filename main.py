a991693d7ca5d87eae329f820643ecf16c23a07f
e2k-vampyre <vip.elick@gmail.com>
2021-04-10 19:47:30 +0500

    fix

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ class LogWindow(QMainWindow, logs.Ui_MainWindow):
         list_file = os.listdir(def_dir)
         for i in list_file:
             filename, file_ext = os.path.splitext(i)
-            if os.path.isdir(def_dir+"/"+i):
+            if os.path.isdir(i):
                 if def_dir+"/"+i != dir_pho and def_dir+"/"+i != dir_vid and def_dir+"/"+i != dir_mus and def_dir+"/"+i != dir_oth:
                     os.replace(def_dir+"/"+i,dir_oth+"/"+i)
                     self.logs_list.addItem(dir_oth+"/"+i)
